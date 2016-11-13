37130b52bc71d3d30c6332dab31fb329b750a02e
GoLok56 <satadii11@gmail.com>
2016-11-13 10:08:04 +0700

    Add files via upload

---
+++ src/Database/Session.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class Session {
+    public static String username; //simpen username, just because
+    public static String name; //simpen nama dari db
+    public static double dompet; //simpen sisa uang didompet dari db
+    public static double saldo; //sisa saldo dari db
+}
